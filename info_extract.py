7fda5676cbc5fdfcb68fbed6402d616bb37e9f47
Tim Garvin <tcgarvin@gmail.com>
2023-12-21 22:12:36 -0500

    info_extract.get_mass_times

--- info_extract.py
+++ info_extract.py
@@ -1,9 +1,24 @@
+import json
 from time import sleep
 from typing import List, IO
 
 from pydantic import BaseModel
 
 from openai import Client
+
+MASSTIME_PROMPT = """What are the regular Mass Times at this Parish?  Provide output as a valid JSON array in which every object in the array represents a single mass time.  Include attributes for the day of the week and the time of day.  The "day" attribute should be the name of the day, and the "time" attribute should be an int representing 24hr time.  (900 is 9am, 1400 is 2pm, etc.)
+
+Example Response:
+
+[
+ {
+    "day": "Sunday",
+    "time": 900
+ }
+]
+
+Do not include any content in the response other than the JSON itself.
+"""
 
 class MassTime(BaseModel):
     day: str  # "Monday"
@@ -19,7 +34,7 @@ def get_mass_times(client:Client, assistant_id:str, bulletin_pdf:IO[bytes]) -> L
     thread = client.beta.threads.create()
     client.beta.threads.messages.create(
         thread_id=thread.id, 
-        content="What are the regular Mass Times at this Parish?  Provide output as a valid JSON array in which every object in the array represents a single mass time.  Include attributes for the day of the week and the time of day.  Do not include any content in the response other than the JSON itself.",
+        content=MASSTIME_PROMPT,
         role="user",
         file_ids=[uploaded_bulletin.id]
     )
@@ -42,11 +57,12 @@ def get_mass_times(client:Client, assistant_id:str, bulletin_pdf:IO[bytes]) -> L
     client.files.delete(uploaded_bulletin.id)
     client.beta.threads.delete(thread.id)
 
-    print(messages.data[0].content[0].text.value)
-
-    MassTime.model_validate_json()
-
-    return []
+    response_string = messages.data[0].content[0].text.value
+    print(response_string)
+    json_str = response_string[response_string.find("[") : response_string.rfind("]") + 1]
+    response_json = json.loads(json_str)
+    response_masstimes = [MassTime.model_validate_json(json.dumps(j)) for j in response_json]
+    return response_masstimes
 
 if __name__ == '__main__':
     # Test code
