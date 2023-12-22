1ba3ea8f1ff407af1ff6b704c598fd6d6ac950c9
Tim Garvin <tcgarvin@gmail.com>
2023-12-21 23:02:03 -0500

    Stub persistance functions

---
+++ database.py
@@ -0,0 +1,10 @@
+from typing import List
+
+from download_bulletins import MassTime
+
+def get_parishes_to_check() -> List[str]:
+    return ["0689"]
+
+def update_parish_masstimes(parish_id:str, masstimes:List[MassTime]):
+    return
+
