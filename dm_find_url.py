8df7fc14e450da95325842b129167c8a387826c7
Michael Garvin <mfjgarvin@gmail.com>
2024-01-10 21:26:33 -0500

    added support for discover mass and ecatholic bulletins

--- dm_find_url.py
+++ dm_find_url.py
@@ -2,33 +2,34 @@ import urllib.request
 from bs4 import BeautifulSoup
 import argparse
 
-def listen_for_arguments():
+def listen_for_arguments(): #This is defunct - not used
     parser = argparse.ArgumentParser(description='Return the URL for the most recent bulletin.')
     parser.add_argument('parish_name', nargs='*', help='Name of the parish from its permalink URL: e.g. st-isidore-kingston')
     return parser.parse_args()
+#args = listen_for_arguments()
 
-args = listen_for_arguments()
-if args.parish_name:
-    print("success")
-    print(args.parish_name[0])
-    url = "https://discovermass.com/church/" + args.parish_name[0] + "/#bulletins"
-    req = urllib.request.Request(
-        url,
-        data=None,
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15'
-        }
-    )
-    rawdata = urllib.request.urlopen(req)
-# rawdata = open("test.html", "r")
-    html = rawdata.read().decode('utf-8')
-    soup = BeautifulSoup(html, "html.parser")
-    bulletin = soup.find(id="bulletin-current")
-    bulletin_link = bulletin.get('href')
-    bulletin_date = bulletin.get_text()
+def get_dm_url(name:str):
+    if name:
+    #    print("success")
+    #    print(args.parish_name[0])
+        url = "https://discovermass.com/church/" + name + "/#bulletins"
+        req = urllib.request.Request(
+            url,
+            data=None,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15'
+            }
+        )
+        rawdata = urllib.request.urlopen(req)
+    # rawdata = open("test.html", "r")
+        html = rawdata.read().decode('utf-8')
+        soup = BeautifulSoup(html, "html.parser")
+        bulletin = soup.find(id="bulletin-current")
+        bulletin_link = bulletin.get('href')
+        bulletin_date = bulletin.get_text()
 
-    print('This bulletin is from ' + bulletin_date + "\n")
-    print(bulletin_link)
+    #    print('This bulletin is from ' + bulletin_date + "\n")
+        return(bulletin_link)
 
-else:
-    print("Enter a parish name and try again")
+    else:
+        raise Exception("Enter a parish name and try again")
--- download_bulletins.py
+++ download_bulletins.py
@@ -1,31 +1,47 @@
 from datetime import datetime, timedelta
 import httpx
 from typing import IO
+from dm_find_url import get_dm_url
+import time
 
 PARISHES_ONLINE_ROOT = "https://container.parishesonline.com/bulletins/14"
 PARISHES_ONLINE_FILE_FORMAT = "%Y%m%dB.pdf"
+ECATHOLIC_ONLINE_ROOT = "https://files.ecatholic.com"
+ECATHOLIC_ONLINE_FILE_FORMAT = "%Y%m%d.pdf"
 DAYS_TO_LOOK_BEFORE_GIVING_UP = 30
 
-def download_bulletin(parish_id:str, file:IO[bytes]):
+def download_bulletin(parish_id:str, file:IO[bytes], publisher_type="PO"):
     """
-    Given a parishesonline parish ID in region 14, download the parish bulletin
+    For Discover Mass:      Given a parish permalink, download the parish bulletin
+    For Parishes Online:    Given a parishesonline parish ID in region 14, download the parish bulletin
+    For eCatholic:          Given an eCatholic parish ID, download the parish bulletin
     """
 
     # Search backwards in time until we find a bulletin
     current_date = datetime.now()
     success = False
+    if publisher_type == "DM":
+        url = get_dm_url(parish_id)
+        response = httpx.get(url)
+        time.sleep(30)
     for i in range(30):
         date_to_check = current_date - timedelta(days=i)
-        filename = date_to_check.strftime(PARISHES_ONLINE_FILE_FORMAT)
-        url = f"{PARISHES_ONLINE_ROOT}/{parish_id}/{filename}"
-        response = httpx.get(url)
+        if publisher_type == "PO": 
+            filename = date_to_check.strftime(PARISHES_ONLINE_FILE_FORMAT)
+            url = f"{PARISHES_ONLINE_ROOT}/{parish_id}/{filename}"
+            response = httpx.get(url)
+        if publisher_type == "EC":
+            filename = date_to_check.strftime(ECATHOLIC_ONLINE_FILE_FORMAT)
+            url = f"{ECATHOLIC_ONLINE_ROOT}/{parish_id}/bulletins/{filename}"
+            response = httpx.get(url)
         #print(url, response.status_code)
         if response.status_code != 200:
             continue
-
+        if response.status_code == 200:
+            file.write(response.content)
+            success = True
+            break
         #print(response.content)
-        file.write(response.content)
-        success = True
 
     if not success:
         raise Exception("No bulletin found")
@@ -33,9 +49,8 @@ def download_bulletin(parish_id:str, file:IO[bytes]):
 if __name__ == "__main__":
     import tempfile
 
-    #with open("test.pdf", "wb") as bulletin_file:
-        #download_bulletin("0689", bulletin_file)
-
+#    with open("test.pdf", "wb") as bulletin_file:
+#        download_bulletin("st-james-the-less-columbus-oh", bulletin_file, "DM")
 
     with tempfile.TemporaryFile("w+b") as bulletin_file:
         download_bulletin("0689", bulletin_file)
